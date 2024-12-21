6823d32753bd0c0b4cf08c880ab5b862b839bc41
ManK0-is-here <zarennyjracok@gmail.com>
2024-12-21 14:22:02 +0300

    1.функция get_weather

--- my_main.py
+++ my_main.py
@@ -1,3 +1,11 @@
 from urllib import response
-import requests
-from plyer import notification # type: ignore
+import requests 
+from plyer import notification 
+
+CITY = "Череповец"
+KEY_API = "23496c2a58b99648af590ee8a29c5348"
+
+def get_weather(city: str = CITY, api_key: str = KEY_API)-> dict:
+    url = rf'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=ru'
+    response: Response = requests.get(url)
+    return requests.json()
