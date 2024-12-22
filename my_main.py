6d713b06e6efd0afbfcfe6d76441707647248254
ManK0-is-here <zarennyjracok@gmail.com>
2024-12-22 12:30:12 +0300

    добавил фугкцию format_wether_message() и исправил зависимости

--- my_main.py
+++ my_main.py
@@ -6,6 +6,27 @@ CITY = "Череповец"
 KEY_API = "23496c2a58b99648af590ee8a29c5348"
 
 def get_weather(city: str = CITY, api_key: str = KEY_API)-> dict:
+    """
+    Выполняет запрос к API и возвращает данные о погоде в виде словаря.
+    """
     url = rf'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=ru'
-    response: Response = requests.get(url)
+    response: requests.Response = requests.get(url)
     return requests.json()
+
+
+def format_weather_message(weather_dict: dict) -> str:
+    """
+    Получает данные из функции get_weather и форматирует их в удобочитаемое сообщение
+    """
+    # weather_dict = get_weather()
+
+    # температура
+    temp = weather_dict['main']['temp']
+    # ощущение температуры
+    feels_like = weather_dict['main']['feels_like']
+    # что на улице
+    description =weather_dict['weather'][0]['description']
+
+    return (f"температура: {temp}, ощущается как: {feels_like}, описание: {description}")
+
+
